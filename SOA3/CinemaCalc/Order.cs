2fed7679b135213304fb0dd8d525a9c90db733d4
Thom Martens <thommartens14@hotmail.com>
2019-01-31 14:47:20 +0100

    first commit

---
+++ SOA3/CinemaCalc/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaCalc
+{
+    public class Order
+    {
+        private int orderNr;
+        private bool isStudentOrder;
+
+        public Order(int orderNr, bool isStudentOrder)
+        {
+            this.orderNr = orderNr;
+            this.isStudentOrder = isStudentOrder;
+        }
+
+    }
+}
